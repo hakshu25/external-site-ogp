1f4fcb0cee7969e261fdf1b4cdd710b75856a940
hakshu <hakshu25@gmail.com>
2024-02-24 20:50:25 +0900

    feat: add ogp api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,47 @@
 import { Hono } from 'hono'
 
+const getContent = (element: Element): string => {
+  return element.getAttribute("content") ?? "";
+}
+
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/ogp', async (c) => {
+  const siteUrl = c.req.query('site_url');
+  if (!siteUrl) {
+    return c.body('site_url query is required', 400)
+  }
+
+  const sitePage = await fetch(decodeURIComponent(siteUrl));
+  if (!sitePage.ok) {
+    return c.body('Not found site page', 404)
+  }
+
+  const responseBody = {
+    title: '',
+    description: '',
+    image: '',
+    siteName: '',
+  }
+
+  await new HTMLRewriter().on("meta[property^='og:']", { element: (element: Element): void => {
+    switch (element.getAttribute("property")) {
+      case "og:title":
+        responseBody.title = getContent(element);
+        break;
+      case "og:description":
+        responseBody.description = getContent(element);
+        break;
+      case "og:image":
+        responseBody.image = getContent(element);
+        break;
+      case "og:site_name":
+        responseBody.siteName = getContent(element);
+    }
+  }}).transform(sitePage).text();
+
+  c.header('Cache-Control', 'public, max-age=3600, s-maxage=3600, stale-while-revalidate=3600, stale-if-error=3600');
+  return c.json(responseBody);
 })
 
 export default app
